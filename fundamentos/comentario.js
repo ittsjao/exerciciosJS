4b7813eb155d25d71223672294035beb87c0c3e7
itsjao <jaocesarsantos@gmail.com>
2023-07-10 07:00:58 -0300

    Added some exercises and notes

---
+++ fundamentos/comentario.js
@@ -0,0 +1,20 @@
+// Comentario de uma linha
+console.log('Linha 1')
+
+/*
+    Comentario de
+    multiplas linhas
+*/
+console.log('Linha 2')
+
+/*
+ * Comentario de
+ * multiplas linhas
+ */
+console.log('Linha 3')
+
+/* 
+1 - Evite comentarios idiotas
+2 - Procure ter nomes que sao claros
+3 - Comente tudo que sentir necessitade de comentar (contexto de aprendizado)
+*/
